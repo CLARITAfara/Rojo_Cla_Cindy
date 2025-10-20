f6211d2b408cdc0c30ce6eb255d584e9bc682ede
CLARITAfara <clarita.razafiarisoa@gmail.com>
2025-10-20 19:31:00 +0300

    Ajout initial du projet Finance Centrale (sans target ni fichiers IDE)

---
+++ src/main/java/finance/FinanceCentraleApplication.java
@@ -0,0 +1,11 @@
+package finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceCentraleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceCentraleApplication.class, args);
+    }
+}
